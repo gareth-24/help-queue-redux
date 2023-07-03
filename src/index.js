2fa4e0c51ac56e7b0ffbc21bf64ff477e5a5d669
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-03 11:13:18 -0700

    update index.js with new imports from reducer and react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/ticket-list-reducer';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
