ee968361e4a528ec66f1698107246f397c312fb2
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-05 09:36:02 -0700

    add passing tests for index-reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
-import { createStore } from 'redux';
 
-let store = createStore(rootReducer);
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   mainTicketList: ticketListReducer
